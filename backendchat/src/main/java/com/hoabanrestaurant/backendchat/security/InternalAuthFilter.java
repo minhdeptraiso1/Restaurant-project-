ff3900768414bdceb1412de69bba522e52bbdacd
Nguyen Quang Minh <nguyenquangminh081003@gmail.com>
2025-11-30 21:19:07 +0700

    add .gitignore

--- backendchat/src/main/java/com/hoabanrestaurant/backendchat/security/InternalAuthFilter.java
+++ backendchat/src/main/java/com/hoabanrestaurant/backendchat/security/InternalAuthFilter.java
@@ -1,11 +1,17 @@
 package com.hoabanrestaurant.backendchat.security;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class InternalAuthFilter implements Filter {
